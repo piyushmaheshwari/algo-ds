46bf3a0e111161a348c01341f92f07d906339ac2
Piyush Maheshwari <piyushisnow@gmail.com>
2016-02-24 21:53:36 -0800

    Printing longest subsequence

--- epi-java/src/dp/LongestIncSubsequence.java
+++ epi-java/src/dp/LongestIncSubsequence.java
@@ -1,47 +1,54 @@
 package dp;
 
-public class LongestIncSubsequence {
+import java.util.*;
 
+public class LongestIncSubSequence {
 
-    int binarySearch(int[] arr, int low, int high, int val) {
-        int index = 0;
+    int binarySearchBetter(int[] arr, int low, int high, int val) {
+        int ans = high + 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (arr[mid] > val) {
+                ans = mid;
                 high = mid - 1;
-            } else { // arr[mid] <= val
-                if ((mid + 1 > high) || (arr[mid + 1] >= val)) {
-                    index = mid + 1;
-                }
+            } else {
                 low = mid + 1;
             }
         }
-        return index;
+        return ans;
     }
 
-    int solve(int[] arr) {
+    List<Integer> solve(int[] arr) {
         int n = arr.length;
         int[] dp = new int[n];
+        int [] index = new int[n];
         int[] parent = new int[n];
         int high = 0;
-        dp [0] = arr [0];
-        for (int i = 1; i < n; i++){
-            int t = binarySearch(dp, 0, high, arr[i]);
-            dp [t] = arr[i];
+        dp[0] = arr[0];
+        parent [0] = -1;
+        index [0] = 0;
+        for (int i = 1; i < n; i++) {
+            int t = binarySearchBetter(dp, 0, high, arr[i]);
+            dp[t] = arr[i];
+            index [t] = i;
+            parent [i] = index [t - 1];
             if (t > high)
                 high = t;
         }
-        for (int i = 0; i < high + 1; i++){
-            System.out.print(dp[i] + " ");
-        }
-        System.out.println("");
 
-        return high + 1;
+        List<Integer> result = new ArrayList<>();
+        int p = index [high];
+        while (p != -1){
+            result.add(arr[p]);
+            p = parent [p];
+        }
+
+        Collections.reverse(result);
+        return result;
     }
 
     public void test() {
         int[] input = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9};
-        //int [] test = {1,3,5};
         System.out.println(solve(input));
     }
 }
